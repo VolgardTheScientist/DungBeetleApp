76da49457df04ad23a6f63d1b86f677486e7d89d
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-07-12 14:55:18 +0200

    Fixing GCS credentials for GCS cloud V2

--- pages/Material Warehouse.py
+++ pages/Material Warehouse.py
@@ -10,6 +10,7 @@ import requests
 from pages.ifc_viewer.ifc_viewer import ifc_viewer
 from google.cloud import storage
 from google.oauth2.service_account import Credentials
+import json
 
 st.set_page_config(layout="wide")
 st.title("Digital material warehouse")
@@ -54,7 +55,8 @@ def download_ifc_file_from_github(ifc_file_name):
     return local_path
 
 def upload_to_gcs(data, bucket_name, blob_name):
-    credentials = Credentials.from_service_account_info(st.secrets["google"]["GOOGLE_APPLICATION_CREDENTIALS"])
+    credentials_json = json.loads(st.secrets["google"]["GOOGLE_APPLICATION_CREDENTIALS"])
+    credentials = Credentials.from_service_account_info(credentials_json)
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     # List all blobs and convert the iterator to a list
