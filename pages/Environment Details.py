48d6cf85109c0388c91fbbd63a2940ec2aebbb4a
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-07-16 19:15:24 +0200

    Check IfcOpenShell version

--- pages/Environment Details.py
+++ pages/Environment Details.py
@@ -9,6 +9,13 @@ import io
 from google.cloud import storage
 from google.oauth2.service_account import Credentials
 import tempfile
+import pkg_resources
+
+# ========== Add versions variable ==========
+
+ifcPenShellVersion = pkg_resources.get_distribution("ifcopenshell").version
+st.write(ifcPenShellVersion)
+
 
 # ========== Page title and welcome message, page config ==========
 
@@ -18,4 +25,5 @@ st.write("")
 st.write("Pandas: ", pd.__version__)
 st.write("Streamlit: ", st.__version__)
 st.write("Numpy: ", np.__version__)
+st.write("IfcOpenShell: ", ifcPenShellVersion)
 
