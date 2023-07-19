3f54e4fbeda12ec38bc9b7838fa907334d19eef3
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-07-19 18:18:16 +0200

    2D in viewer

--- pages/ifc_viewer/app.js
+++ pages/ifc_viewer/app.js
@@ -15,6 +15,17 @@ function setFrameHeight(height) {
     sendMessageToStreamlitClient("streamlit:setFrameHeight", {height});
 }
 
+function toggleShadow(active) {
+    const shadows = Object.values(viewer.shadowDropper.shadows);
+    for (shadow of shadows) {
+      shadow.root.visible = active;
+    }
+  }
+  
+  function togglePostproduction(active) {
+    viewer.context.renderer.postProduction.active = active;
+  }
+
 const viewer = new IfcViewerAPI({container: document.getElementById('viewer-container'), backgroundColor: new Color(0xffffff)});
 viewer.axes.setAxes();
 viewer.grid.setGrid();
@@ -25,7 +36,54 @@ async function loadIfc(url) {
   
     // Add dropped shadow and post-processing effect
     await viewer.shadowDropper.renderShadow(model.modelID);
-    viewer.context.renderer.postProduction.active = true;  
+
+    await viewer.plans.computeAllPlanViews(model.modelID);
+
+    // Floor plan navigation 
+
+    const lineMaterial = new LineBasicMaterial({ color: "black" });
+    const baseMaterial = new MeshBasicMaterial({
+      polygonOffset: true,
+      polygonOffsetFactor: 1, // positive value pushes polygon further away
+      polygonOffsetUnits: 1,
+    });
+
+    viewer.edges.create(
+      "example-edges",
+      model.modelID,
+      lineMaterial,
+      baseMaterial
+    );
+    
+    const allPlans = viewer.plans.getAll(model.modelID);
+    
+    const container = document.getElementById("button-container");
+    
+    for (const plan of allPlans) {
+      const currentPlan = viewer.plans.planLists[model.modelID][plan];
+      console.log(currentPlan);
+    
+      const button = document.createElement("button");
+      container.appendChild(button);
+      button.textContent = currentPlan.name;
+      button.onclick = () => {
+        viewer.plans.goTo(model.modelID, plan);
+        viewer.edges.toggle("example-edges", true);
+        togglePostproduction(false);
+        toggleShadow(false);
+      };
+    }
+  
+    const button = document.createElement("button");
+    container.appendChild(button);
+    button.textContent = "Exit floorplans";
+    button.onclick = () => {
+      viewer.plans.exitPlanView();
+      viewer.edges.toggle("example-edges", false);
+      togglePostproduction(true);
+      toggleShadow(true);
+    };
+
   }
 
 function onDataFromPython(event) {
@@ -75,28 +133,3 @@ window.addEventListener("load", function() {
         setFrameHeight(500)
     }, 0);
 });
-
-// Floor plan viewing
-
-const allPlans = viewer.plans.getAll(model.modelID);
-
-for (const plan of allPlans) {
-    const currentPlan = viewer.plans.planLists[model.modelID][plan];
-    console.log(currentPlan);
-
-    const buttonPlan = document.createElement('button');
-    container.appendChild(buttonPlan);
-    buttonPlan.textContent = currentPlan.name;
-    buttonPlan.onclick = () => {
-        viewer.plans.goTo(model.modelID, plan);
-        viewer.edges.toggle('example', true);
-    };
-}
-
-const buttonExit = document.createElement('button');
-container.appendChild(buttonExit);
-buttonExit.textContent = 'Exit';
-buttonExit.onclick = () => {
-    viewer.plans.exitPlanView();
-    viewer.edges.toggle('example', false);
-};
