3787483b7849f9eadbd8d7e53eeb20220ae61638
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-08-16 10:56:29 +0200

    Changed main app structure to accommodate login

--- pages/Warehouse Admin.py
+++ pages/Warehouse Admin.py
@@ -29,40 +29,6 @@ credentials = Credentials.from_service_account_info(st.secrets["GOOGLE_APPLICATI
 storage_client = storage.Client(credentials=credentials)
 
 
-# ========== Protect content with password ==========
-
-# Custom session state
-if 'logged_in' not in st.session_state:
-    st.session_state.logged_in = False
-
-def check_password(password_input):
-    correct_password = st.secrets["ADMIN_CREDENTIALS"]["password"]
-    if password_input == correct_password:
-        return True
-    return False
-
-def main_app():
-    """
-    Your main app goes here
-    """
-    st.title("Welcome to the Protected App!")
-    st.write("You are now logged in and can see this content.")
-
-# If not logged in, show login. Else show main app.
-if not st.session_state.logged_in:
-    st.title("Please log in")
-    password_input = st.text_input("Enter Password", type='password')
-
-    if st.button("Login"):
-        if check_password(password_input):
-            st.session_state.logged_in = True
-            main_app()
-        else:
-            st.warning("Incorrect password. Please try again.")
-else:
-    main_app()
-
-
 # ========== Function definitions ==========
 
 def save_to_bucket(uploaded_file, blob_name):
@@ -354,3 +320,29 @@ def main_app():
 
     if "rerun_page" in st.session_state and st.session_state["rerun_page"] == "no":
         st.write("", st.session_state["uploaded_ifc_file"])
+
+# ========== Protect content with password ==========
+
+# Custom session state
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+
+def check_password(password_input):
+    correct_password = st.secrets["ADMIN_CREDENTIALS"]["password"]
+    if password_input == correct_password:
+        return True
+    return False
+
+# If not logged in, show login. Else show main app.
+if not st.session_state.logged_in:
+    st.title("Please log in")
+    password_input = st.text_input("Enter Password", type='password')
+
+    if st.button("Login"):
+        if check_password(password_input):
+            st.session_state.logged_in = True
+            main_app()
+        else:
+            st.warning("Incorrect password. Please try again.")
+else:
+    main_app()
