1114eba623a6d894ab3a6a4f099a5e41263de221
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-08-25 14:30:47 +0200

    Password management - access Heroku Secrets
    First attempt to access Heroku, if it fails proceed to st.secrets

--- pages/HerokuPasswords.py
+++ pages/HerokuPasswords.py
@@ -4,16 +4,23 @@ import os
 
 # ========== Fetch SECRETS ==========
 
-try:
-    # Try to get the password using Streamlit's built-in secrets management
-    correct_password = st.secrets["ADMIN_CREDENTIALS"]["password"]
-except (FileNotFoundError, KeyError):
-    # If that fails, try to get the password from the Heroku environment variable
-    toml_string = os.environ.get("SECRETS_TOML", "")
-    if toml_string:
-        parsed_toml = toml.loads(toml_string)
-        correct_password = parsed_toml.get("ADMIN_CREDENTIALS", {}).get("password", "")
+# Initialize correct_password to an empty string
+correct_password = ""
+
+# First, try to get the password from the Heroku environment variable
+toml_string = os.environ.get("SECRETS_TOML", "")
+if toml_string:
+    parsed_toml = toml.loads(toml_string)
+    correct_password = parsed_toml.get("ADMIN_CREDENTIALS", {}).get("password", "")
+
+# If the above fails, then try to get the password using Streamlit's built-in secrets management
+if not correct_password:
+    try:
+        correct_password = st.secrets["ADMIN_CREDENTIALS"]["password"]
+    except (FileNotFoundError, KeyError):
+        pass  # Handle the exception gracefully or log an appropriate message if needed
 
 # Now you can use `correct_password` in your code
+
 
 st.write(correct_password)
