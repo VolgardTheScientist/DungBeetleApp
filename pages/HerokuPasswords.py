f50bacde88bcaaf418c205d403adc97b6dba90cd
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-08-25 14:07:13 +0200

    Password management - Heroku test file

---
+++ pages/HerokuPasswords.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import toml
+import os
+
+# ========== Fetch SECRETS ==========
+
+try:
+    # Try to get the password using Streamlit's built-in secrets management
+    correct_password = st.secrets["ADMIN_CREDENTIALS"]["password"]
+except KeyError:
+    # If that fails, try to get the password from the Heroku environment variable
+    toml_string = os.environ.get("SECRETS_TOML", "")
+    if toml_string:
+        parsed_toml = toml.loads(toml_string)
+        correct_password = parsed_toml.get("ADMIN_CREDENTIALS", {}).get("password", "")
+
+# Now you can use `correct_password` in your code
+
+st.write(correct_password)
